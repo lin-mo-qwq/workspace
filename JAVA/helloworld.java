acd9bc92de80711c92a80e7bb3fbef8414fd2d0e
zxly <2823696074@qq.com>
2020-05-14 23:14:17 +0800

    add java

--- JAVA/helloworld.java
+++ JAVA/helloworld.java
@@ -1,9 +1,11 @@
 package JAVA;
 
+import java.io.IOException;
+
 public class helloworld 
 {
-	public static void main( String[] args) 
+	public static void main( final String[] args) throws IOException 
 	{
-		System.out.println("Helloworld");
+		System.out.print("Helloworld");
 	}
 }
