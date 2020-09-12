448c9f4fc33175bf126f79c0047518a948ad9ba7
lin-mo-qwq <2823696074@qq.com>
2020-09-12 15:03:52 +0800

    111

---
+++ luogu/C++/P2568.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define clr(x) memset(x, true, sizeof(x))
+#define M 10000005
+
+typedef long long ll;
+int tot, phi[M], pri[M], p;
+ll sum[M], ans;
+bool ispri[M];
+
+void work(int x)
+{
+	clr(ispri);
+	phi[1] = 1;
+	for(int i = 2; i <= x; i++)
+	{
+		
+		if(ispri[i]) pri[++tot] = i, phi[i] = i - 1;
+		for(int j = 1; j <= tot && i * pri[j] <= x; j++)
+		{
+			ispri[i * pri[j]] = false;
+			if(i % pri[j] == 0)
+			{
+				phi[i * pri[j]] = phi[i] * pri[j];
+				break;
+			}
+			else
+				phi[i * pri[j]] = phi[i] * phi[pri[j]];
+		}
+	}
+	for(int i = 1; i <= x; i++) sum[i] = sum[i - 1] + phi[i];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	work(n);
+	for(int i = 1; i <= tot; i++)	ans += 2 * sum[n / pri[i]] - 1;
+	cout << ans << endl;
+	return 0;
+}
